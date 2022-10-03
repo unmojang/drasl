dc3e3d2a48720467b53562d318585ad071afa5f0
Evan Goode <mail@evangoo.de>
2022-10-03 19:45:00 -0400

    style tweaks

--- js/background.ts
+++ js/background.ts
@@ -12,16 +12,16 @@ uniform sampler2D u_texture;
 const float TIME_SCALE = 1.0 / 30.0;
 
 mat4 rotateAxis(vec3 axis, float angle) {
-    // From https://www.neilmendoza.com/glsl-rotation-about-an-arbitrary-axis/
-    axis = normalize(axis);
-    float s = sin(angle);
-    float c = cos(angle);
-    float oc = 1.0 - c;
-    
-    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
-                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
-                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
-                0.0,                                0.0,                                0.0,                                1.0);
+	// From https://www.neilmendoza.com/glsl-rotation-about-an-arbitrary-axis/
+	axis = normalize(axis);
+	float s = sin(angle);
+	float c = cos(angle);
+	float oc = 1.0 - c;
+	
+	return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+	            oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+	            oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+	            0.0,                                0.0,                                0.0,                                1.0);
 }
 
 void main() {
@@ -57,67 +57,67 @@ uniform sampler2D u_texture;
 const float TIME_SCALE = 1.0 / 1500.0;
 
 const vec3[] COLORS = vec3[](
-    vec3(0.022087, 0.098399, 0.110818),
-    vec3(0.011892, 0.095924, 0.089485),
-    vec3(0.027636, 0.101689, 0.100326),
-    vec3(0.046564, 0.109883, 0.114838),
-    vec3(0.064901, 0.117696, 0.097189),
-    vec3(0.063761, 0.086895, 0.123646),
-    vec3(0.084817, 0.111994, 0.166380),
-    vec3(0.097489, 0.154120, 0.091064),
-    vec3(0.106152, 0.131144, 0.195191),
-    vec3(0.097721, 0.110188, 0.187229),
-    vec3(0.133516, 0.138278, 0.148582),
-    vec3(0.070006, 0.243332, 0.235792),
-    vec3(0.196766, 0.142899, 0.214696),
-    vec3(0.047281, 0.315338, 0.321970),
-    vec3(0.204675, 0.390010, 0.302066),
-    vec3(0.080955, 0.314821, 0.661491)
+	vec3(0.022087, 0.098399, 0.110818),
+	vec3(0.011892, 0.095924, 0.089485),
+	vec3(0.027636, 0.101689, 0.100326),
+	vec3(0.046564, 0.109883, 0.114838),
+	vec3(0.064901, 0.117696, 0.097189),
+	vec3(0.063761, 0.086895, 0.123646),
+	vec3(0.084817, 0.111994, 0.166380),
+	vec3(0.097489, 0.154120, 0.091064),
+	vec3(0.106152, 0.131144, 0.195191),
+	vec3(0.097721, 0.110188, 0.187229),
+	vec3(0.133516, 0.138278, 0.148582),
+	vec3(0.070006, 0.243332, 0.235792),
+	vec3(0.196766, 0.142899, 0.214696),
+	vec3(0.047281, 0.315338, 0.321970),
+	vec3(0.204675, 0.390010, 0.302066),
+	vec3(0.080955, 0.314821, 0.661491)
 );
 
 const mat4 SCALE_TRANSLATE = mat4(
-    0.5, 0.0, 0.0, 0.25,
-    0.0, 0.5, 0.0, 0.25,
-    0.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0
+	0.5, 0.0, 0.0, 0.25,
+	0.0, 0.5, 0.0, 0.25,
+	0.0, 0.0, 1.0, 0.0,
+	0.0, 0.0, 0.0, 1.0
 );
 
 mat2 mat2_rotate_z(float radians) {
-    return mat2(
-        cos(radians), -sin(radians),
-        sin(radians), cos(radians)
-    );
+	return mat2(
+	    cos(radians), -sin(radians),
+	    sin(radians), cos(radians)
+	);
 }
 
 mat4 end_portal_layer(float layer) {
-    mat4 translate = mat4(
-        1.0, 0.0, 0.0, 17.0 / layer,
-        0.0, 1.0, 0.0, (2.0 + layer / 1.5) * (TIME_SCALE * u_time * -1.5),
-        0.0, 0.0, 1.0, 0.0,
-        0.0, 0.0, 0.0, 1.0
-    );
+	mat4 translate = mat4(
+	    1.0, 0.0, 0.0, 17.0 / layer,
+	    0.0, 1.0, 0.0, (2.0 + layer / 1.5) * (TIME_SCALE * u_time * -1.5),
+	    0.0, 0.0, 1.0, 0.0,
+	    0.0, 0.0, 0.0, 1.0
+	);
 
-    mat2 rotate = mat2_rotate_z(3.3 + radians((layer * layer * 4321.0 + layer * 9.0) * 2.0));
+	mat2 rotate = mat2_rotate_z(3.3 + radians((layer * layer * 4321.0 + layer * 9.0) * 2.0));
 
-    mat2 scale = mat2((4.5 - layer / 4.0) * 2.0);
+	mat2 scale = mat2((4.5 - layer / 4.0) * 2.0);
 
-    return mat4(scale * rotate) * translate * SCALE_TRANSLATE;
+	return mat4(scale * rotate) * translate * SCALE_TRANSLATE;
 }
 
 out vec4 fragColor;
 
 void main() {
-    vec3 color = vec3(0, 0, 0);
-    float s = 1.5;
-    float lightness = 0.75;
-    mat2 texcoordScale = mat2(
-	20.0 * s, 0.0,
-	0.0, 1.0 * s
-    );
-    for (int i = 0; i < 16; i++) {
-        color += textureProj(u_texture, vec4(v_texcoord * texcoordScale, 0.0, 1.0) * end_portal_layer(float(i + 1))).rgb * COLORS[i] * lightness;
-    }
-    fragColor = vec4(color, 1.0);
+	vec3 color = vec3(0, 0, 0);
+	float s = 1.5;
+	float lightness = 0.75;
+	mat2 texcoordScale = mat2(
+		20.0 * s, 0.0,
+		0.0, 1.0 * s
+	);
+	for (int i = 0; i < 16; i++) {
+	    color += textureProj(u_texture, vec4(v_texcoord * texcoordScale, 0.0, 1.0) * end_portal_layer(float(i + 1))).rgb * COLORS[i] * lightness;
+	}
+	fragColor = vec4(color, 1.0);
 }
 `;
 
