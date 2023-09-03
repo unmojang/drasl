e361538f85a295fba5835c77eaa21a3b75fe5002
Evan Goode <mail@evangoo.de>
2023-09-03 17:17:31 -0400

    NixOS module

--- flake.nix
+++ flake.nix
@@ -10,90 +10,122 @@
     };
   };
 
-  outputs = { self, nixpkgs, npmlock2nix }:
-    let
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+  outputs = {
+    self,
+    nixpkgs,
+    npmlock2nix,
+  }: let
+    version = "0.9.3";
 
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 lastModifiedDate;
+    # System types to support.
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
 
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system:
-        let
-          overlays = [
-            (final: prev: {
-              npmlock2nix = import npmlock2nix {pkgs = prev;};
-            })
-          ];
-        in import nixpkgs { inherit system overlays; });
-
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: let
+      overlays = [
+        (final: prev: {
+          npmlock2nix = import npmlock2nix {pkgs = prev;};
+        })
+      ];
     in
-    {
+      import nixpkgs {inherit system overlays;});
+  in {
+    # Provide some binary packages for selected system types.
+    packages = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+      nodeModules = pkgs.npmlock2nix.v2.node_modules {
+        src = ./.;
+        nodejs = pkgs.nodejs;
+      };
+    in {
+      drasl = pkgs.buildGoModule {
+        pname = "drasl";
+        inherit version;
+        src = ./.;
 
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-          nodeModules = pkgs.npmlock2nix.v2.node_modules {
-            src = ./.;
-            nodejs = pkgs.nodejs;
-          };
-        in
-        {
-          drasl = pkgs.buildGoModule {
-            pname = "drasl";
-            inherit version;
-            # In 'nix develop', we don't need a copy of the source tree
-            # in the Nix store.
-            src = ./.;
+        # Update whenever Go dependencies change
+        vendorSha256 = "sha256-YNOTEza43Uvl2FaU4DiFo3GLmnn/o106pMnHyjQ+Je4=";
 
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake sha256 and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remeber to bump this hash when your dependencies change.
-            vendorSha256 = "sha256-YNOTEza43Uvl2FaU4DiFo3GLmnn/o106pMnHyjQ+Je4=";
+        outputs = ["out"];
 
-            outputs = [ "out" ];
+        preConfigure = ''
+          substituteInPlace build_config.go --replace "\"/usr/share/drasl\"" "\"$out/share/drasl\""
+        '';
 
-            preConfigure = ''
-              substituteInPlace build_config.go --replace "\"/usr/share/drasl\"" "\"$out/share/drasl\""
+        preBuild = ''
+          ln -s ${nodeModules}/node_modules node_modules
+          ${pkgs.nodejs}/bin/node esbuild.config.js
+          cp css/style.css public/
+        '';
+
+        postInstall = ''
+          mkdir -p "$out/share/drasl"
+          cp -R ./{assets,view,public} "$out/share/drasl"
+        '';
+      };
+    });
+
+    nixosModules.drasl = {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+      with lib; let
+        cfg = config.services.drasl;
+        format = pkgs.formats.toml {};
+      in {
+        options.services.drasl = {
+          enable = mkEnableOption (lib.mdDoc ''drasl'');
+          settings = mkOption {
+            type = format.type;
+            default = {};
+            description = lib.mdDoc ''
+              config.toml for drasl
             '';
-
-            preBuild = ''
-              ln -s ${nodeModules}/node_modules node_modules
-              ${pkgs.nodejs}/bin/node esbuild.config.js
-              cp css/style.css public/
-            '';
-
-            postInstall = ''
-              mkdir -p "$out/share/drasl"
-              cp -R ./{assets,view,public} "$out/share/drasl"
-            '';
           };
-        });
+        };
+        config = mkIf cfg.enable {
+          systemd.services.drasl = {
+            description = "drasl";
+            wantedBy = ["multi-user.target"];
 
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [ go gopls gotools go-tools sqlite-interactive nodejs golangci-lint pre-commit delve gore ];
+            serviceConfig = let
+              pkg = self.defaultPackage.${pkgs.system};
+              config = format.generate "config.toml" cfg.settings;
+            in {
+              ExecStart = "${pkg}/bin/drasl -config ${config}";
+              DynamicUser = true;
+              StateDirectory = "drasl";
+              Restart = "always";
+            };
           };
-        });
+        };
+      };
 
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.drasl);
-    };
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          alejandra
+          delve
+          go
+          go-tools
+          golangci-lint
+          gopls
+          gore
+          gotools
+          nodejs
+          pre-commit
+          sqlite-interactive
+        ];
+      };
+    });
+
+    defaultPackage = forAllSystems (system: self.packages.${system}.drasl);
+  };
 }
