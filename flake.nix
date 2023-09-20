c637ad83a6f93e18eb63477e271ca0452f64c508
Evan Goode <mail@evangoo.de>
2023-09-19 22:49:42 -0400

    Bump flake version to 1.0.0

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     nixpkgs,
     npmlock2nix,
   }: let
-    version = "0.9.4";
+    version = "1.0.0";
 
     # System types to support.
     supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
