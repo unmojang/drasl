7ae4a1ac8627a6db65f1555432cd0956ff4245fe
Evan Goode <mail@evangoo.de>
2024-08-15 15:52:48 -0400

    Implement joinserver.jsp and checkserver.jsp

--- session.go
+++ session.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 type sessionJoinRequest struct {
@@ -41,6 +42,60 @@ func SessionJoin(app *App) func(c echo.Context) error {
 	}
 }
 
+// /game/joinserver.jsp
+func SessionJoinServer(app *App) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		username := c.QueryParam("user")
+		sessionID := c.QueryParam("sessionId")
+		serverID := c.QueryParam("serverId")
+
+		// If any parameters are missing, return NO
+		if username == "" || sessionID == "" || serverID == "" {
+			return c.String(http.StatusOK, "Bad login")
+
+		}
+
+		// Parse sessionId. It has the form:
+		// token:<accessToken>:<player UUID>
+		split := strings.Split(sessionID, ":")
+		if len(split) != 3 || split[0] != "token" {
+			return c.String(http.StatusOK, "Bad login")
+		}
+		accessToken := split[1]
+		id := split[2]
+
+		// Is the accessToken valid?
+		client := app.GetClient(accessToken, StalePolicyDeny)
+		if client == nil {
+			return c.String(http.StatusOK, "Bad login")
+		}
+
+		// If the player name corresponding to the access token doesn't match
+		// the `user` param from the request, return NO
+		user := client.User
+		if user.PlayerName != username {
+			return c.String(http.StatusOK, "Bad login")
+		}
+		// If the player's UUID doesn't match the UUID in the sessionId, return
+		// NO
+		userID, err := UUIDToID(user.UUID)
+		if err != nil {
+			return err
+		}
+		if userID != id {
+			return c.String(http.StatusOK, "Bad login")
+		}
+
+		user.ServerID = MakeNullString(&serverID)
+		result := app.DB.Save(&user)
+		if result.Error != nil {
+			return result.Error
+		}
+
+		return c.String(http.StatusOK, "OK")
+	}
+}
+
 func fullProfile(app *App, user *User, uuid string, sign bool) (SessionProfileResponse, error) {
 	id, err := UUIDToID(uuid)
 	if err != nil {
@@ -59,67 +114,84 @@ func fullProfile(app *App, user *User, uuid string, sign bool) (SessionProfileRe
 	}, nil
 }
 
+func (app *App) hasJoined(c *echo.Context, playerName string, serverID string, legacy bool) error {
+	var user User
+	result := app.DB.First(&user, "player_name = ?", playerName)
+	// If the error isn't "not found", throw.
+	if result.Error != nil && !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return result.Error
+	}
+
+	if result.Error != nil || !user.ServerID.Valid || serverID != user.ServerID.String {
+		for _, fallbackAPIServer := range app.Config.FallbackAPIServers {
+			if fallbackAPIServer.DenyUnknownUsers && result.Error != nil {
+				// If DenyUnknownUsers is enabled and the player name is
+				// not known, don't query the fallback server.
+				continue
+			}
+			base, err := url.Parse(fallbackAPIServer.SessionURL)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			base.Path += "/session/minecraft/hasJoined"
+			params := url.Values{}
+			params.Add("username", playerName)
+			params.Add("serverId", serverID)
+			base.RawQuery = params.Encode()
+
+			res, err := MakeHTTPClient().Get(base.String())
+			if err != nil {
+				log.Printf("Received invalid response from fallback API server at %s\n", base.String())
+				continue
+			}
+			defer res.Body.Close()
+
+			if res.StatusCode == http.StatusOK {
+				if legacy {
+					return (*c).String(http.StatusOK, "YES")
+				} else {
+					return (*c).Stream(http.StatusOK, res.Header.Get("Content-Type"), res.Body)
+				}
+			}
+		}
+
+		if legacy {
+			return (*c).String(http.StatusMethodNotAllowed, "NO")
+		} else {
+			return (*c).NoContent(http.StatusForbidden)
+		}
+	}
+
+	if legacy {
+		return (*c).String(http.StatusOK, "YES")
+	}
+
+	profile, err := fullProfile(app, &user, user.UUID, true)
+	if err != nil {
+		return err
+	}
+
+	return (*c).JSON(http.StatusOK, profile)
+}
+
 // /session/minecraft/hasJoined
 // https://c4k3.github.io/wiki.vg/Protocol_Encryption.html#Server
 func SessionHasJoined(app *App) func(c echo.Context) error {
 	return func(c echo.Context) error {
 		playerName := c.QueryParam("username")
 		serverID := c.QueryParam("serverId")
+		return app.hasJoined(&c, playerName, serverID, false)
+	}
+}
 
-		var user User
-		result := app.DB.First(&user, "player_name = ?", playerName)
-		if result.Error != nil && !errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			if app.Config.TransientUsers.Allow && app.TransientUsernameRegex.MatchString(playerName) {
-				var err error
-				user, err = MakeTransientUser(app, playerName)
-				if err != nil {
-					return err
-				}
-			} else {
-				return c.NoContent(http.StatusForbidden)
-			}
-		}
-
-		if result.Error != nil || !user.ServerID.Valid || serverID != user.ServerID.String {
-			for _, fallbackAPIServer := range app.Config.FallbackAPIServers {
-				if fallbackAPIServer.DenyUnknownUsers && result.Error != nil {
-					// If DenyUnknownUsers is enabled and the player name is
-					// not known, don't query the fallback server.
-					continue
-				}
-				base, err := url.Parse(fallbackAPIServer.SessionURL)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-
-				base.Path += "/session/minecraft/hasJoined"
-				params := url.Values{}
-				params.Add("username", playerName)
-				params.Add("serverId", serverID)
-				base.RawQuery = params.Encode()
-
-				res, err := MakeHTTPClient().Get(base.String())
-				if err != nil {
-					log.Printf("Received invalid response from fallback API server at %s\n", base.String())
-					continue
-				}
-				defer res.Body.Close()
-
-				if res.StatusCode == http.StatusOK {
-					return c.Stream(http.StatusOK, res.Header.Get("Content-Type"), res.Body)
-				}
-			}
-
-			return c.NoContent(http.StatusForbidden)
-		}
-
-		profile, err := fullProfile(app, &user, user.UUID, true)
-		if err != nil {
-			return err
-		}
-
-		return c.JSON(http.StatusOK, profile)
+// /game/checkserver.jsp
+func SessionCheckServer(app *App) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		playerName := c.QueryParam("user")
+		serverID := c.QueryParam("serverId")
+		return app.hasJoined(&c, playerName, serverID, true)
 	}
 }
 
