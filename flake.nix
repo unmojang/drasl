d0ac20f8e7f8778b88979ebea6a839f10f19ae78
Evan Goode <mail@evangoo.de>
2024-11-22 15:46:56 -0500

    Bump version to 2.1.0

--- flake.nix
+++ flake.nix
@@ -14,14 +14,14 @@
     nixpkgs,
     buildNodeModules,
   }: let
-    version = "2.0.2";
+    version = "2.1.0";
 
     supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
 
     # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-    overlays = [ ];
+    overlays = [];
 
     nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system overlays;});
     nixpkgsCross =
