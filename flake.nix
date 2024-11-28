ce942717ca44cbd1a2cba9b83c96726471d6261b
Evan Goode <mail@evangoo.de>
2024-11-28 12:43:28 -0500

    Bump version to 3.0.0

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     nixpkgs,
     buildNodeModules,
   }: let
-    version = "2.1.0";
+    version = "3.0.0";
 
     supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
 
