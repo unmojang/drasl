fa8f8114cead95998da2e17b643276d9e73f19ee
xllifi <unicoded@ya.ru>
2025-02-02 22:03:01 +1000

    remove APIRegisterChallenge due to unnecessary

--- main.go
+++ main.go
@@ -194,7 +194,6 @@ func (app *App) MakeServer() *echo.Echo {
 
 	e.POST(DRASL_API_PREFIX+"/login", app.APILogin())
 	e.POST(DRASL_API_PREFIX+"/register", app.APIRegister())
-	e.POST(DRASL_API_PREFIX+"/register-challenge", app.APIRegisterChallenge())
 	e.POST(DRASL_API_PREFIX+"/invites", app.APICreateInvite())
 	e.POST(DRASL_API_PREFIX+"/players", app.APICreatePlayer())
 	e.POST(DRASL_API_PREFIX+"/users", app.APICreateUser())
